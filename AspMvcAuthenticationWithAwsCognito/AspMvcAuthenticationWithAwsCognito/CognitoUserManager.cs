67615b2ad20f36b84b5d6dafaf76305ccd736718
Nlb <nilbertrr@gmail.com>
2018-04-25 18:22:09 -0400

    -adding UserManager.

---
+++ AspMvcAuthenticationWithAwsCognito/AspMvcAuthenticationWithAwsCognito/CognitoUserManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Amazon.CognitoIdentityProvider;
+using Amazon.CognitoIdentityProvider.Model;
+using Microsoft.AspNet.Identity;
+
+namespace POC.CognitoAuth
+{
+
+    public class CognitoUserManager : UserManager<CognitoUser>
+    {
+
+        private readonly AmazonCognitoIdentityProviderClient _client =
+            new AmazonCognitoIdentityProviderClient();
+        private readonly string _clientId = ConfigurationManager.AppSettings["CLIENT_ID"];
+        private readonly string _poolId = ConfigurationManager.AppSettings["USERPOOL_ID"];
+
+        public CognitoUserManager(IUserStore<CognitoUser> store)
+            : base(store)
+        {
+        }
+        public override Task<bool> CheckPasswordAsync(CognitoUser user, string password)
+        {
+            return CheckPasswordAsync(user.UserName, password);
+        }
+
+        private async Task<bool> CheckPasswordAsync(string userName, string password)
+        {
+            try
+            {
+                var authReq = new AdminInitiateAuthRequest
+                {
+                    UserPoolId = ConfigurationManager.AppSettings["USERPOOL_ID"],
+                    ClientId = ConfigurationManager.AppSettings["CLIENT_ID"],
+                    AuthFlow = AuthFlowType.ADMIN_NO_SRP_AUTH
+                };
+                authReq.AuthParameters.Add("USERNAME", userName);
+                authReq.AuthParameters.Add("PASSWORD", password);
+
+                AdminInitiateAuthResponse authResp = await _client.AdminInitiateAuthAsync(authReq);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
