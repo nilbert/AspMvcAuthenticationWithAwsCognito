306c4e5496bb6d190dd630adb01379074e6a19fc
Nlb <nilbertrr@gmail.com>
2018-04-25 18:00:27 -0400

    -add Cognito User.

---
+++ AspMvcAuthenticationWithAwsCognito/AspMvcAuthenticationWithAwsCognito/CognitoUser.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.CognitoIdentityProvider;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AspMvcAuthenticationWithAwsCognito
+{
+    public class CognitoUser : IdentityUser
+    {
+        public string Password { get; set; }
+        public UserStatusType Status { get; set; }
+
+        //public string Roles { get; set; }
+    }
+}
